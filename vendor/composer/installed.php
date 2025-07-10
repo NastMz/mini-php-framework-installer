6dc24a2da1c204172c02b64aa62d86393f55a514
Kevin Martinez <ksmartinez23@outlook.com>
2025-07-09 22:25:49 -0500

    bump version to 1.0.1 and update author information

--- vendor/composer/installed.php
+++ vendor/composer/installed.php
@@ -1,9 +1,9 @@
 <?php return array(
     'root' => array(
         'name' => 'miniframework/installer',
-        'pretty_version' => 'dev-main',
-        'version' => 'dev-main',
-        'reference' => '7b459b7739697436ba58d3e7284337738dbb6117',
+        'pretty_version' => '1.0.1',
+        'version' => '1.0.1.0',
+        'reference' => null,
         'type' => 'library',
         'install_path' => __DIR__ . '/../../',
         'aliases' => array(),
@@ -11,9 +11,9 @@
     ),
     'versions' => array(
         'miniframework/installer' => array(
-            'pretty_version' => 'dev-main',
-            'version' => 'dev-main',
-            'reference' => '7b459b7739697436ba58d3e7284337738dbb6117',
+            'pretty_version' => '1.0.1',
+            'version' => '1.0.1.0',
+            'reference' => null,
             'type' => 'library',
             'install_path' => __DIR__ . '/../../',
             'aliases' => array(),
